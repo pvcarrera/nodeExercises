a70ae56ce024b47b7660c25b91a0ce5714391f0f
Pablo Vicente <vicapa99@gmail.com>
2012-09-10 00:04:22 +0100

    Create express server

---
+++ serverExpress.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express.createServer();
+
+app.get('/', function(request, response){
+    response.sendfile(__dirname + "/index.html");
+});
+
+app.listen(8080);
