b05f3eb7404a429c45da58a9a956458aeb2b97a1
Pablo Vicente <vicapa99@gmail.com>
2012-09-16 19:54:24 +0100

    Add ejs template example

--- serverExpress.js
+++ serverExpress.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express.createServer();
 
-app.get('/', function(request, response){
-    response.sendfile(__dirname + "/index.html");
-});
+var quotes = {
+    'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+     'berners-lee': 'The Web does not just connect machines, it connects people',
+     'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+     'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+};
 
+app.get('/quotes/:name', function(request, response) {
+      var quote = quotes[request.params.name];
+      response.render(__dirname + '/quote.ejs', {name: request.params.name, block: quote})
+});
 app.listen(8080);
